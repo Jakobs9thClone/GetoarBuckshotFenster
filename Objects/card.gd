57644cf3e9199f36a45c2907e0370ad000a128c0
Jakob <jakob@citystation.net>
2025-11-10 21:17:33 +0100

    absoluter commit
    Absoluter Commit

--- Objects/card.gd
+++ Objects/card.gd
@@ -14,3 +14,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		print("Mouse Click/Unclick at: ", event.position)
+	elif event is InputEventMouseMotion:
+		#print("Mouse Motion at: ", event.position)
+		pass
+
+	# Print the size of the viewport.
+	print("Viewport Resolution is: ", get_viewport().get_visible_rect().size)
