35f23c2a148f9f613ee6145bffd8fd99f6fff4ec
Jakobs9thClone <jakob@citystation.net>
2025-11-10 15:54:49 +0100

    > Gumo ihr Pisser

---
+++ Objects/card.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var isActive = false
+@export var isCorner = true
+
+var cardValue = 0; #max 12
+var cardSymbol = 0; #max 3
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
