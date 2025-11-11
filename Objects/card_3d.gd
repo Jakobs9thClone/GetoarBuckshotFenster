fc747aec829a809115e65212f84eea581f77e604
Jakob <jakob@citystation.net>
2025-11-11 22:35:55 +0100

    Dings kp was ich alles gemacht hab

--- Objects/card_3d.gd
+++ Objects/card_3d.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@export var debug_Label: Node
 @export var isActive = false
 @export var isCorner = true
 @export var topCard: Node
@@ -10,16 +11,18 @@ extends Node3D
 var cardValue = 0; #max 12
 var cardSymbol = 0; #max 3
 
-var hasBeenTurned = false;
+var hasBeenTurned = false
 var hasTurnedNeighbours = false
 
-var mouseIsOn = false;
+var mouseIsOn = false
 
 var otherVar
 
 @onready var animation_player = $AnimationPlayer
 
 func _ready() -> void:
+	cardValue = randi_range(0,12)
+	debug_Label.text = str(cardValue)
 	if isCorner:
 		hasBeenTurned =  true
 		animation_player.play("flip")
@@ -42,15 +45,34 @@ func _process(delta: float) -> void:
 		if rightCard.hasBeenTurned == true:
 			hasTurnedNeighbours = true
 
+func getAmountOfTurnedNeighbours() -> int:
+	var temp = 0
+	if topCard != null:
+		if topCard.hasBeenTurned == true:
+			temp += 1
+	if bottomCard != null:
+		if bottomCard.hasBeenTurned == true:
+			temp += 1
+	if leftCard != null:
+		if leftCard.hasBeenTurned == true:
+			temp += 1
+	if rightCard != null:
+		if rightCard.hasBeenTurned == true:
+			temp += 1
+	return temp
+
+func reset() -> void:
+	pass
+
 func _input(event):
-	# Mouse in viewport coordinates.
 	if event is InputEventMouseButton:
+		if event.is_action_pressed("Mouse Left"):
+			pass
 		if mouseIsOn and not hasBeenTurned:
 			hasBeenTurned = true
 			animation_player.play("flip")
 	elif event is InputEventMouseMotion:
 		pass
-
 
 func _on_area_3d_mouse_entered() -> void:
 	if not hasBeenTurned and hasTurnedNeighbours:
