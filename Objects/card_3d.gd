d213d9f54b1a4d5609eb76dfb4338f03aaa81be1
Jakob <jakob@citystation.net>
2025-11-11 14:40:22 +0100

    dings

---
+++ Objects/card_3d.gd
@@ -0,0 +1,48 @@
+extends Node3D
+
+@export var isActive = false
+@export var isCorner = true
+@export var topCard: Node
+@export var bottomCard: Node
+@export var leftCard: Node
+@export var rightCard: Node
+
+var cardValue = 0; #max 12
+var cardSymbol = 0; #max 3
+
+var mouseIsOn = false;
+
+var otherVar
+
+
+func _ready() -> void:
+	pass
+	#var otherVar = $card
+	#Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if mouseIsOn == true:
+		print("otherCard active")
+	#print(otherVar.isActive)
+	#var otherScript = otherCard.get_script()
+	#if otherScript.isActive == true:
+	#	print("collision with other object")
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		print("Mouse Click/Unclick at: ", event.position)
+	elif event is InputEventMouseMotion:
+		#print("Mouse Motion at: ", event.position)
+		pass
+
+	# Print the size of the viewport.
+	#print("Viewport Resolution is: ", get_viewport().get_visible_rect().size)
+
+func _on_area_3d_mouse_entered() -> void:
+	mouseIsOn = true; # Replace with function body.
+
+
+func _on_area_3d_mouse_exited() -> void:
+	mouseIsOn = false # Replace with function body.
