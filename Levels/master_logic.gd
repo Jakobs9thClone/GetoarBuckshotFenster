39a2f5704331b8f4ecc3764290ebe60bfc669fe1
Jakob <jakob@citystation.net>
2025-11-12 22:23:42 +0100

    commit weil muss

--- Levels/master_logic.gd
+++ Levels/master_logic.gd
@@ -10,7 +10,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _input(event):
+	if event.is_action_pressed("Mouse Middle"):
+		if Fenster.currentPlayer == 0:
+			cameraAnimator.play("camera_turn")
+			Fenster.currentPlayer = 1
+		elif Fenster.currentPlayer == 1:
+			cameraAnimator.play("camera_return")
+			Fenster.currentPlayer = 0
 
+	
 func _on_button_pressed() -> void: #weitergeben Knopf
 	if Fenster.currentPlayer == 0:
 		cameraAnimator.play("camera_turn")
