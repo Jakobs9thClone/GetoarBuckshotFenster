a5db28513ad179cb2e96f0d9c0270b2b24196673
Jakob <jakob@citystation.net>
2025-11-12 19:17:49 +0100

    Karten sind random hingelegt, weitergeben feature angefangen

---
+++ Levels/master_logic.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@export var cameraAnimator: AnimationPlayer
+@export var Fenster: Node
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void: #weitergeben Knopf
+	if Fenster.currentPlayer == 0:
+		cameraAnimator.play("camera_turn")
+		Fenster.currentPlayer = 1
+	elif Fenster.currentPlayer == 1:
+		cameraAnimator.play("camera_return")
+		Fenster.currentPlayer = 0
