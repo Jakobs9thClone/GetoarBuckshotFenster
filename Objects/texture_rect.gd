069cd032fd70eb598d253cd7912719cb34d8116f
elmo <schmerbacheric@gmail.com>
2025-11-12 11:13:26 +0100

    komplette background + logik dikka

--- Objects/texture_rect.gd
+++ Objects/texture_rect.gd
@@ -1,9 +1,4 @@
 extends TextureRect
 
-@export var parallax_strength: float = 0.05
-
-func _process(delta):
-	var mouse_pos = get_viewport().get_mouse_position()
-	var win_size = get_viewport_rect().size
-	var offset = -(mouse_pos - win_size * 0.5) * parallax_strength
-	position = offset
+func _process(_delta):
+	position = get_viewport_rect().size * 0.5 - size * 0.5 - (get_viewport().get_mouse_position() - get_viewport_rect().size * 0.5) * 0.05
